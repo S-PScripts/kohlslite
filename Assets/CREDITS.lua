446229537383ec16aa2ed5685eab4b7c90265923
S-PScripts <ts2021me@outlook.com>
2025-01-08 21:41:36 +0000

    Create CREDITS.lua

---
+++ Assets/CREDITS.lua
@@ -0,0 +1,33 @@
+print("--- INFORMATION ---")
+print("KohlsLite, since 2023. Created by ScriptingProgrammer/ts2021")
+print("For help, please contact me on Discord at ts2021")
+print("The version you are using is v"..getgenv().klversion..". This build was created on 8th January 2024")
+print("Better than SCV1 and SCV2 - approved by Tech!")
+print("\n")
+
+print("--- CREDITS ---")
+print([[
+ -- > CMD (v1) [by quiving]
+ -- > CMD Y [by quiving]
+ -- > CMD v3 (Pi) [by quiving]
+ -- > Shortcut v1
+ -- > Shortcut v2
+ -- > Shortcut v3
+ -- > Shortcut v3 VAR [don't have the source] [by Tech]
+ -- > ii's Stupid Admin [by iiDk]
+ -- > PR Script old [by Atprog]
+ -- > KohlsNoob
+ -- > KohlsCool
+ -- > Noobsploit
+ -- > Jotunnheim [by Jotunn]
+ -- > Shazam [by Tokio]
+ -- > Route [by Dizzy]
+ -- > SimpleKAH [by lnfiniteCoder]
+ -- > XKah [by lnfiniteCoder]
+ -- > Solinium [by Knocks]
+ -- > Infinite Yield
+ -- > Proton Admin [by Digitality]
+ -- > Many scripts from the KAH script archive by Damix - View it here: S-PScripts/kah-fork ]])
+print("\n")
+
+print("VIEW THE SOURCE HERE: kohlslite.pages.dev/source.lua")
