fbebb2fa5a4fcf9c41b156b21a1f15bcc373a106
s-pscripts (ts2021) <ts2021me@outlook.com>
2025-03-26 19:25:54 +0000

    Update source.lua

--- source.lua
+++ source.lua
@@ -15,6 +15,7 @@ Before you view the source, here are some things you might want to know:
 - This script also already has the most antis so I won't add antis for individual players/rewrite the anti system.
 
 TS2021 20/03/2025
+Update 26/03/2025: NP version was taken down, it'll be back soon though..
 ]]
 
 --[[
