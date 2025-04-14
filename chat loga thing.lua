8b351998ffe9154b7d61b9f50c236bfe5d9ecea9
s-pscripts (ts2021) <ts2021me@outlook.com>
2025-04-14 16:26:14 +0100

    Create chat loga thing.lua

---
+++ chat loga thing.lua
@@ -0,0 +1,14 @@
+function PLRSTART(v)
+v.Chatted:Connect(function(msg)
+task.wait(0)
+print(msg)
+end)
+end
+
+game.Players.PlayerAdded:Connect(function(player)
+PLRSTART(player)
+end)
+
+for i, v in pairs(game.Players:GetPlayers()) do
+PLRSTART(v)
+end
