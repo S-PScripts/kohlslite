0df772d58f77e46806338f181b5eb4eafafb3b0c
s-pscripts (ts2021) <ts2021me@outlook.com>
2025-04-14 16:26:54 +0100

    Update and rename chat loga thing.lua to uh/chat loga thing.lua

--- uh/chat loga thing.lua
+++ uh/chat loga thing.lua
@@ -1,14 +1,15 @@
+-- chat loga thing
 function PLRSTART(v)
-v.Chatted:Connect(function(msg)
-task.wait(0)
-print(msg)
-end)
+  v.Chatted:Connect(function(msg)
+    task.wait(0)
+    print(msg)
+  end)
 end
 
 game.Players.PlayerAdded:Connect(function(player)
-PLRSTART(player)
+  PLRSTART(player)
 end)
 
 for i, v in pairs(game.Players:GetPlayers()) do
-PLRSTART(v)
+  PLRSTART(v)
 end
