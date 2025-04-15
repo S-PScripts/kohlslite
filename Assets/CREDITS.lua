0ba315034aadb7dddafe92a643b6ea8466750a32
s-pscripts (ts2021) <ts2021me@outlook.com>
2025-04-15 20:17:37 +0100

    Update CREDITS.lua

--- Assets/CREDITS.lua
+++ Assets/CREDITS.lua
@@ -1,33 +1,34 @@
 print("--- INFORMATION ---")
-print("KohlsLite, since 2024. Created by ScriptingProgrammer/ts2021")
+print("KohlsLite is a script for Kohls Admin House that was made in early 2024. This script was created by ScriptingProgrammer (Roblox)/ts2021 (Discord)/S-PScripts (GitHub).")
 print("For help, please contact me on Discord at ts2021")
-print("The version you are using is v"..getgenv().klversion..". This build was created on 2nd April 2025.")
+print("The version you are using is v"..getgenv().klversion..". This build was created on 15th April 2025.")
 print("Better than SCV1 and SCV2 - approved by Tech!")
 print("\n")
 
 print("--- CREDITS ---")
 print([[
- -- > CMD (v1) [by quiving]
- -- > CMD Y [by quiving]
- -- > CMD v3 (Pi) [by quiving]
- -- > Shortcut v1
- -- > Shortcut v2
- -- > Shortcut v3
- -- > Shortcut v3 VAR [don't have the source] [by Tech]
- -- > ii's Stupid Admin [by iiDk]
- -- > PR Script old [by Atprog]
- -- > KohlsNoob
- -- > KohlsCool
- -- > Noobsploit
- -- > Jotunnheim [by Jotunn]
- -- > Shazam [by Tokio]
- -- > Route [by Dizzy]
- -- > SimpleKAH [by lnfiniteCoder]
- -- > XKah [by lnfiniteCoder]
- -- > Solinium [by Knocks]
- -- > Infinite Yield
- -- > Proton Admin [by Digitality]
- -- > Many scripts from the KAH script archive by Damix - View it here: S-PScripts/kah-fork ]])
+KohlsLite is a bit like a mixture of all the scripts that already exist in KAH such as:
+  -- > CMD (v1) [by quiving, the same person that made the Solara executor]
+  -- > CMD Y [by quiving]
+  -- > CMD v3 (Pi) [by quiving]
+  -- > Shortcut v1 [by SnowClan_8342/yeemi]
+  -- > Shortcut v2 [by SnowClan_8342/yeemi]
+  -- > Shortcut v3 [by Tech]
+  -- > Shortcut v3 VAR [don't have the source] [by Tech]
+  -- > ii's Stupid Admin [by iiDk]
+  -- > Old PR Script [by atprog]
+  -- > KohlsNoob [by gamingkhoaito#1014 and haroldjd2017ipad#4295]
+  -- > KohlsCool [by sergioesquina/kohlscool]
+  -- > Noobsploit [by NoobSploit]
+  -- > Jotunnheim [by Jotunn]
+  -- > Shazam [by Tokio]
+  -- > Route [by Dizzy]
+  -- > SimpleKAH [by lnfiniteCoder]
+  -- > XKah [by lnfiniteCoder]
+  -- > Solinium [by Knocks]
+  -- > Infinite Yield [by EdgeIY]
+  -- > Proton Admin [by Digitality]
+  -- > Many scripts from the KAH script archive by Damix [View it here: S-PScripts/kah-fork] ]])
 print("\n")
 
 print("For more information, view the source here: kohlslite.pages.dev/source.lua (run source command to copy to clipboard).")
