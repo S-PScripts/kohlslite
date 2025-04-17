a6d2bb9649f801211575d12739603d6758959ba3
s-pscripts (ts2021) <ts2021me@outlook.com>
2025-04-17 15:13:21 +0100

    Update source-np.lua

--- source-np.lua
+++ source-np.lua
@@ -1,1 +1,15 @@
 -- kohlslite np
+local function Chat(msg)
+	game:GetService("Players"):Chat(msg)
+end
+
+local function Remind(text, num)
+	if num == nil then num = 1 end
+	game:GetService("StarterGui"):SetCore("SendNotification",
+	{
+		Title = "KohlsLite NP",
+		Text = text,
+		Duration = num
+	}
+	)
+end
