3f15b6521b36914c13829b270030748e945d7838
s-pscripts (ts2021) <ts2021me@outlook.com>
2025-04-19 12:28:13 +0100

    Update PLAYERTOKEN.lua

--- Assets/PLAYERTOKEN.lua
+++ Assets/PLAYERTOKEN.lua
@@ -5,3 +5,8 @@ else
 end
 -- Once you have copied the code above, join an empty server and run the code. Next, open a text editor like Notepad and find a string that looks like this: '5568CCBED82CD30E127119030810CE98'.
 -- Once you have found the string, copy it and input it into the playertoken variable.
+
+--[[
+Example:
+{"previousPageCursor":null,"nextPageCursor":null,"data":[{"id":"fae6b9b9-29a2-49b4-96ca-197ddcbc36a4","maxPlayers":8,"playing":1,"playerTokens":["F5EDE1379E3D63C1E07059C82BB8FCD0"],"players":[],"fps":59.997513,"ping":167}]}
+]]
