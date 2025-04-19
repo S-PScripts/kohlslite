367b2a58c3e846901d53f45c59b0efb8d3265267
s-pscripts (ts2021) <ts2021me@outlook.com>
2025-04-19 12:27:04 +0100

    Update PLAYERTOKEN.lua

--- Assets/PLAYERTOKEN.lua
+++ Assets/PLAYERTOKEN.lua
@@ -1,3 +1,7 @@
-game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true")
+if setclipboard then
+  setclipboard(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true"))
+else
+  print(game:HttpGet("https://games.roblox.com/v1/games/"..game.PlaceId.."/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true"))
+end
 -- Once you have copied the code above, join an empty server and run the code. Next, open a text editor like Notepad and find a string that looks like this: '5568CCBED82CD30E127119030810CE98'.
 -- Once you have found the string, copy it and input it into the playertoken variable.
