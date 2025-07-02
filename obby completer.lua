9c4d9f4441a87836719864c127741e58bec9aab1
s-pscripts (ts2021) <ts2021me@outlook.com>
2025-07-02 08:48:26 +0100

    Update obby completer.lua

--- obby completer.lua
+++ obby completer.lua
@@ -1,4 +1,10 @@
 -- for no jump obby
+-- well, this probably works for other games like it as well
+-- it could be better but idgaf
+
+start_at = 1 -- where it should start teleporting from
+number_of_checkpoints = 241 -- set this to the number of checkpoints in the game
+teleport_time = 0.2 -- time before teleporting to each checkpoint
 local player = game.Players.LocalPlayer
 
 local function teleport_to_checkpoints()
@@ -6,11 +12,11 @@ local function teleport_to_checkpoints()
 	local hrp = character:WaitForChild("HumanoidRootPart")
 	local checkpoints = workspace:WaitForChild("Checkpoints")
 
-	for i = 1, 241 do
+	for i = start_at, number_of_checkpoint do
 		local checkpoint = checkpoints:FindFirstChild(tostring(i))
 		if checkpoint then
 			hrp.CFrame = checkpoint.CFrame + Vector3.new(0, 3, 0)
-			wait(0.2)
+			wait(teleport_time)
 		end
 	end
 end
