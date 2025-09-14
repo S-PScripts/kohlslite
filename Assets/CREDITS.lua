4046d3b93d17f0df4993773bab83df4779b3de42
s-pscripts (ts2021) <ts2021me@outlook.com>
2025-09-14 14:01:55 +0100

    Update CREDITS.lua

--- Assets/CREDITS.lua
+++ Assets/CREDITS.lua
@@ -1,7 +1,7 @@
 print("--- INFORMATION ---")
 print("KohlsLite is a script for Kohls Admin House that was created in late 2023. This script was created by ScriptingProgrammer (Roblox)/ts2021 (Discord)/S-PScripts (GitHub).")
 print("For help, please contact me on Discord at ts2021")
-print("The version you are using is v"..getgenv().klversion..". This is the final version, as KohlsLite was discontinued in June 2025.")
+print("The version you are using is v"..getgenv().klversion..". 14/09/2025")
 print("\n")
 
 print("--- CREDITS ---")
