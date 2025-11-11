501eb68456151ee4dff0c5c08b21c0e1b572b391
S-PScripts <ts2021me@outlook.com>
2025-11-11 08:54:37 +0000

    Create mainx.lua

---
+++ mainx.lua
@@ -0,0 +1,14 @@
+--[[
+PrisonX
+
+A script for Aesthetical's game Prison Life.
+Created by ScriptingProgrammer (Roblox) / ts2021 (Discord) / S-PScripts (GitHub)
+View the source here: https://kohlslite.pages.dev/PrisonX.lua
+]]
+
+
+
+-- Loadstring
+loadstring(game:HttpGet("https://kohlslite.pages.dev/PrisonX.lua"))()
+
+-- More settings can be found in the script's source itself.
