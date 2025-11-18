e51fb62ca8cd8e194b4044144bf1b7980d36f527
S-PScripts <ts2021me@outlook.com>
2025-11-18 22:25:16 +0000

    Update mainx.lua

--- mainx.lua
+++ mainx.lua
@@ -1,14 +1,16 @@
 --[[
-PrisonX
-
+██████╗ ██████╗ ██╗███████╗ ██████╗ ███╗   ██╗██╗  ██╗
+██╔══██╗██╔══██╗██║██╔════╝██╔═══██╗████╗  ██║╚██╗██╔╝
+██████╔╝██████╔╝██║███████╗██║   ██║██╔██╗ ██║ ╚███╔╝ 
+██╔═══╝ ██╔══██╗██║╚════██║██║   ██║██║╚██╗██║ ██╔██╗ 
+██║     ██║  ██║██║███████║╚██████╔╝██║ ╚████║██╔╝ ██╗
+╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝
 A script for Aesthetical's game Prison Life.
 Created by ScriptingProgrammer (Roblox) / ts2021 (Discord) / S-PScripts (GitHub)
 View the source here: https://kohlslite.pages.dev/PrisonX.lua
 ]]
 
-
-
 -- Loadstring
 loadstring(game:HttpGet("https://kohlslite.pages.dev/PrisonX.lua"))()
 
--- More settings can be found in the script's source itself.
+-- Settings can be found in the script's source itself.
